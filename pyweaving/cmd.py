4e2874975f832d977f8a34a974734fae2047e7d1
Scott Torborg <scott@cartlogic.com>
2014-12-28 16:20:43 -0800

    For now, just make the 'pyweaving' command only show a draft image

--- pyweaving/cmd.py
+++ pyweaving/cmd.py
@@ -7,6 +7,6 @@ from .render import ImageRenderer
 def main(args=sys.argv):
     filename = args[1]
     draft = WIFReader(filename).read()
-    out_filename = filename.rsplit('.', 1)[0] + '.png'
-    print "Saving to %s" % out_filename
-    ImageRenderer(draft).save(out_filename)
+    # out_filename = filename.rsplit('.', 1)[0] + '.png'
+    # print "Saving to %s" % out_filename
+    ImageRenderer(draft).show()
