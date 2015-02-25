33c0170957d817f9d30053527c1b5c5b689c114f
Scott Torborg <scott@cartlogic.com>
2015-02-24 19:38:02 -0800

    Cleanup imports and add missing __future__ imports

--- pyweaving/__init__.py
+++ pyweaving/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
 import datetime
 import json
 from copy import deepcopy
--- pyweaving/generators/raster.py
+++ pyweaving/generators/raster.py
@@ -1,3 +1,6 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 from .. import Draft
 from PIL import Image
 
