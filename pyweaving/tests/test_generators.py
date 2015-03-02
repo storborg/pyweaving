40c473bc9af9a05edeef1098a3ac93d7af493028
Scott Torborg <scott@cartlogic.com>
2015-03-01 16:48:47 -0800

    Start dithered gradient test

--- pyweaving/tests/test_generators.py
+++ pyweaving/tests/test_generators.py
@@ -3,10 +3,17 @@ from __future__ import (absolute_import, division, print_function,
 
 from unittest import TestCase
 
-from ..generators import twill, dither, raster, tartan
+from .. import Color
+from ..generators import twill, dither
 
 
 class TestGenerators(TestCase):
     def test_plain_twill(self):
         draft = twill.twill(2)
         self.assertEqual(len(draft.shafts), 4)
+
+    def test_dithered_gradient(self):
+        start = Color((0, 0, 0))
+        end = Color((255, 255, 255))
+        colors = dither.dithered_gradient(start, end, 10)
+        self.assertEqual(len(colors), 10)
