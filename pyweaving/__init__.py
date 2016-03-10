c5ec47e8e040f6db3582d444a716239486742c07
Scott Torborg <storborg@gmail.com>
2016-03-10 11:04:30 -0800

    Prepare version 0.0.8.dev for next development cycle

--- pyweaving/__init__.py
+++ pyweaving/__init__.py
@@ -7,7 +7,7 @@ from copy import deepcopy
 from collections import defaultdict
 
 
-__version__ = '0.0.7'
+__version__ = '0.0.8.dev'
 
 
 class Color(object):
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='pyweaving',
-      version='0.0.7',
+      version='0.0.8.dev',
       description='Python Weaving Tools',
       long_description='',
       classifiers=[
